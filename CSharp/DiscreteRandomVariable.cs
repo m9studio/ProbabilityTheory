fc9306ea2ba5615798dd96074227d9974d8dcf9a
mina987 <44808807+m9studio@users.noreply.github.com>
2022-10-27 17:21:39 +0300

    Update DiscreteRandomVariable.cs

--- CSharp/DiscreteRandomVariable.cs
+++ CSharp/DiscreteRandomVariable.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ru.m9studio.ProbabilityTheory
+namespace m9studio.ProbabilityTheory
 {
     /// <summary>
     /// Discrete Random Variable.
